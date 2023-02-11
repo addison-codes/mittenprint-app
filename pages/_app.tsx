9cf8de569c6c6b5ffed3b3fcc3437d6a8941b318
Addison Wanlass <hi@addison.codes>
2023-02-10 22:22:19 -0500

    chore: cleanup deps and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
-import MainLayout from '../components/layouts/MainLayout'
 import type { AppProps } from 'next/app'
-import { Auth0Provider } from '@auth0/auth0-react';
 import { Flowbite } from 'flowbite-react';
 import theme from '../styles/flowbite-theme'
 import NavbarSidebarLayout from '../components/layouts/NavbarSidebar';
@@ -9,17 +7,12 @@ import NavbarSidebarLayout from '../components/layouts/NavbarSidebar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Auth0Provider
-    domain="dev-ut93f2j5.us.auth0.com"
-    clientId="PYP7p44tmtp2xvuoOBe0zhxfaQcf3PYt"
-    redirectUri={'http://localhost:3000/'}
-    >
+
       <Flowbite theme={{ theme }}>
       <NavbarSidebarLayout>
         <Component {...pageProps} />
       </NavbarSidebarLayout>
       </Flowbite>
-    </Auth0Provider>
   )
 }
 
