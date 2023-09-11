9280e2d2a65d2ae9089a728d2f0a24a5ec51b8cd
Addison Wanlass <hi@addison.codes>
2023-09-11 15:00:11 -0400

    fix: protect page

--- pages/route-selection.jsx
+++ pages/route-selection.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import useSWR from 'swr'
 import RouteTable from '../components/RouteTable'
 import { useState } from 'react';
+import Title from '../components/Title'
+
 
 export default function Home() {
   const [publication, setPublication] = useState()
@@ -24,19 +26,28 @@ const handleChange = e => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className="container mx-auto mt-6">
-        <h1 className="mb-4 text-2xl text-red-800">Create a Route</h1>
-        <h2 className='mb-4 text-xl'>Select a Publication</h2>
-        <select onChange={handleChange} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-6">
-        {publications?.map( pub => {
-          return <option key={pub.id} value={pub.id}>{pub.publicationName}</option>
-          }
-        )
-        }
-        </select>
-        <RouteTable publicationId={publication} />
-
-      </section>
+      {session?.user.role === undefined ? (
+        <Title text={'You must be signed in to access this data'} />
+      ) : (
+        <section className="container mx-auto mt-6">
+          <h1 className="mb-4 text-2xl text-red-800">Create a Route</h1>
+          <h2 className="mb-4 text-xl">Select a Publication</h2>
+          <select
+            onChange={handleChange}
+            id="countries"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-6"
+          >
+            {publications?.map((pub) => {
+              return (
+                <option key={pub.id} value={pub.id}>
+                  {pub.publicationName}
+                </option>
+              )
+            })}
+          </select>
+          <RouteTable publicationId={publication} />
+        </section>
+      )}
     </div>
   )
 }
